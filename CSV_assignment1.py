0975a57f9200a8b6d21a628f2753dc04ff6dcecd
Gideon <shuofeng.sheng@gmail.com>
2020-02-07 23:10:54 -0600

    Feb7

--- CSV_assignment1.py
+++ CSV_assignment1.py
@@ -23,13 +23,13 @@ for row in csv_file_dv:
     try:
         high_dv= int(row[4])
         low_dv = int(row[5])
-        current_date_dv = datetime.strptime(row[2],'%Y-%m-%d')
+        current_date = datetime.strptime(row[2],'%Y-%m-%d')
     except ValueError:
-        print (f"Missing Data for {current_date_dv}")
+        print (f"Missing Data for {current_date}")
     else:
         highs_dv.append(high_dv)
         lows_dv.append(low_dv)
-        dates_dv.append(current_date_dv)
+        dates_dv.append(current_date)
 
 
 
@@ -37,17 +37,15 @@ for row in csv_file_sk:
     try:
         high_sk= int(row[4])
         low_sk = int(row[5])
-        current_date_sk = datetime.strptime(row[2],'%Y-%m-%d')
+        current_date = datetime.strptime(row[2],'%Y-%m-%d')
     except ValueError:
-        print (f"Missing Data for {current_date_sk}")
+        print (f"Missing Data for {current_date}")
     else:
         highs_sk.append(high_sk)
         lows_sk.append(low_sk)
-        dates_sk.append(current_date_sk)
+        dates_sk.append(current_date)
 
-
-
-fig, ax = plt.subplots(2,1)
+fig1 = plt.figure()
 
 plt.plot(dates_dv, highs_dv, color='red', alpha=0.5)
 plt.plot(dates_dv, lows_dv, color='blue',alpha=0.5)
@@ -57,6 +55,18 @@ plt.xlabel("",fontsize=10)
 plt.ylabel("Temperature(F)",fontsize=12)
 plt.tick_params(axis='both',which='major',labelsize=12)
 
-fig.autofmt_xdate()
+fig1.autofmt_xdate()
+
+fig2 = plt.figure()
+
+plt.plot(dates_sk, highs_sk, color='red', alpha=0.5)
+plt.plot(dates_sk, lows_sk, color='blue',alpha=0.5)
+plt.fill_between(dates_sk,highs_sk,lows_sk, facecolor='blue',alpha=0.1)
+plt.title("Daily high temps for Death Valley", fontsize=16)
+plt.xlabel("",fontsize=10)
+plt.ylabel("Temperature(F)",fontsize=12)
+plt.tick_params(axis='both',which='major',labelsize=12)
+
+fig2.autofmt_xdate()
 
 plt.show()

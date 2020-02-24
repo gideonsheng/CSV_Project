d25e1a29aaa6b6ccb26c89212e1aeeaa76f5e0ff
Gideon <shuofeng.sheng@gmail.com>
2020-02-24 14:10:00 -0600

    test access

--- CSV_assignment1.py
+++ CSV_assignment1.py
@@ -2,71 +2,58 @@ import matplotlib.pyplot as plt
 import csv
 from datetime import datetime
 
-open_file_dv = open("death_valley_2018_simple.csv", "r")
-open_file_sk = open("sitka_weather_2018_simple.csv", "r")
-
-csv_file_dv = csv.reader (open_file_dv, delimiter = ",")
-csv_file_sk = csv.reader (open_file_sk, delimiter = ",")
-
-header_row_dv = next(csv_file_dv)
-header_row_sk = next(csv_file_sk)
-
-highs_dv = []
-dates_dv = []
-lows_dv = []
-
-highs_sk = []
-dates_sk = []
-lows_sk = []
-
-for row in csv_file_dv:
-    try:
-        high_dv= int(row[4])
-        low_dv = int(row[5])
-        current_date = datetime.strptime(row[2],'%Y-%m-%d')
-    except ValueError:
-        print (f"Missing Data for {current_date}")
-    else:
-        highs_dv.append(high_dv)
-        lows_dv.append(low_dv)
-        dates_dv.append(current_date)
 
 
+def fileplot(T):                                            
+    import csv
+    from datetime import datetime
+    open_file = open(T, "r")
+    csv_file = csv.reader(open_file, delimiter=",")
+    header_row =next(csv_file)
+    highs =[]
+    dates =[]
+    lows=[]
+    for row in csv_file:
+        try:
+            high = int(row[header_row.index('TMAX')])
+            low =int(row[header_row.index('TMIN')])
+            current__date = datetime.strptime(row[header_row.index('DATE')],'%Y-%m-%d')
+        except ValueError:
+            print(f"missing data for {current__date}")
+        else:
+            highs.append(high) 
+            lows.append(low)
+            dates.append(current__date)
+        name= row[1]
+    open_file.close() 
+    return highs,lows,dates,name
 
-for row in csv_file_sk:
-    try:
-        high_sk= int(row[4])
-        low_sk = int(row[5])
-        current_date = datetime.strptime(row[2],'%Y-%m-%d')
-    except ValueError:
-        print (f"Missing Data for {current_date}")
-    else:
-        highs_sk.append(high_sk)
-        lows_sk.append(low_sk)
-        dates_sk.append(current_date)
+def pltdef (y,Q,W,E,filename):                             
+    
+    y.plot(Q,W,color='blue',alpha=0.5)
+    y.plot(Q,E,color='red', alpha=0.5)
+    y.fill_between(Q,E,W, facecolor='blue', alpha =0.5)
+    y.set_title ((filename[0:-4]), fontsize =16)
+    y.set_xlabel ("", fontsize=10)
+    y.set_ylabel ("Temperature (F) ", fontsize =12)
 
-fig1 = plt.figure()
-
-plt.plot(dates_dv, highs_dv, color='red', alpha=0.5)
-plt.plot(dates_dv, lows_dv, color='blue',alpha=0.5)
-plt.fill_between(dates_dv,highs_dv,lows_dv, facecolor='blue',alpha=0.1)
-plt.title("Daily high temps for Death Valley", fontsize=16)
-plt.xlabel("",fontsize=10)
-plt.ylabel("Temperature(F)",fontsize=12)
-plt.tick_params(axis='both',which='major',labelsize=12)
-
-fig1.autofmt_xdate()
-
-fig2 = plt.figure()
-
-plt.plot(dates_sk, highs_sk, color='red', alpha=0.5)
-plt.plot(dates_sk, lows_sk, color='blue',alpha=0.5)
-plt.fill_between(dates_sk,highs_sk,lows_sk, facecolor='blue',alpha=0.1)
-plt.title("Daily high temps for Death Valley", fontsize=16)
-plt.xlabel("",fontsize=10)
-plt.ylabel("Temperature(F)",fontsize=12)
-plt.tick_params(axis='both',which='major',labelsize=12)
-
-fig2.autofmt_xdate()
-
-plt.show()
+def main():
+    import matplotlib.pyplot as plt
+    
+    filenames=["sitka_weather_2018_simple.csv","death_valley_2018_simple.csv"]          
+    
+    plotaxis=['axis','axis1']                                                   
+    fig, (plotaxis[0],plotaxis[1]) = plt.subplots(2,1)    
+    names=[]
+    i=0
+    for file in filenames:
+        file_highs,file_lows, dates,filenam = fileplot(file)
+        pltdef(plotaxis[i],dates,file_lows,file_highs,filenam)    
+        fig.autofmt_xdate()
+        i+=1
+        names.append(filenam)
+    chart_title = 'Temperature Comparison between ' + names[0] + ' and ' + names[1]
+    fig.suptitle(chart_title, fontsize=16) 
+    plt.show()
+    
+main()
